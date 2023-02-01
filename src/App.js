8d5d8e99ab1e945c1c242e17c968300473c1e7ea
nekkhel <nikhilerukala99@gmail.com>
2023-02-01 16:52:02 +0530

    conditional-rendering-login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react"
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { isLoggedIn: true }
+
+  render() {
+    const { isLoggedIn } = this.state
+    let authButton
+    if (isLoggedIn) {
+      authButton = <button>Logout</button>
+    } else {
+      authButton = <button>Login</button>
+    }
+    return (
+      <div className="container">
+        <h1>React JS</h1>
+        {authButton}
+      </div>
+    )
+ }
 }
 
-export default App;
+export default App
